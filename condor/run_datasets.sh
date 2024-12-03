be6248f4b182ec73e3e46a96bcfe58df5a58ee7e
Nikolaos Plastiras <nipla31@gmail.com>
2024-12-03 13:40:09 +0100

    Added script that runs multiple datasets at once

---
+++ condor/run_datasets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the dataset list file is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <dataset_list_file>"
+    exit 1
+fi
+
+dataset_list_file="$1"
+
+# Check if the dataset list file exists
+if [ ! -f "$dataset_list_file" ]; then
+    echo "Error: File '$dataset_list_file' not found."
+    exit 1
+fi
+
+# Read the dataset list and call batch_submission.sh for each dataset
+while IFS= read -r dataset; do
+    if [ -n "$dataset" ]; then # Skip empty lines
+        ./batch_submission.sh "$dataset"
+
+        wait
+    fi
+done < "$dataset_list_file"
