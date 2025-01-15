320ba81fbb6d361c20a65aaee1456635383820dd
Nikolaos Plastiras <nipla31@gmail.com>
2025-01-15 14:13:30 +0100

    Updated the plotting scripts

--- plotters/eff_vs_run_plots.py
+++ plotters/eff_vs_run_plots.py
@@ -4,38 +4,25 @@ import argparse
 import re
 import math
 import json
+import utils
+from utils import *
 
-ROOT.gStyle.SetOptStat(0)
-ROOT.gStyle.SetLegendBorderSize(0)
-
-latex = ROOT.TLatex()
-latex.SetTextSize(0.04)
-latex.SetTextFont(42)
-ROOT.gStyle.SetLegendTextSize(0.04)
-
-# Colors
-color_0 = ROOT.TColor.GetColor(87,144,252) #blue
-color_1 = ROOT.TColor.GetColor(248,156,32) #orange
-color_2 = ROOT.TColor.GetColor(228,37,54) #red
-color_3 = ROOT.TColor.GetColor(150,74,139) #purple
-color_4 = ROOT.TColor.GetColor(156,156,161) #gray
-color_5 = ROOT.TColor.GetColor(122,33,221) #purple
-
-#parse arguments
+# Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--legend', type=str, help='dataset legend')
 parser.add_argument('-o', type=str, help='output dir')
 parser.add_argument('-i', type=str, help='input dir dir')
 args = parser.parse_args()
 
-dataset_legend = args.legend
+# Pass arguments
 output_dir = args.o
 input_dir = args.i
+# utils.merge_root_files(input_dir)
 
 in_file = ROOT.TFile(input_dir + "merged_total.root","READ")
-c = ROOT.TCanvas("c","c",800,800)
-c.SetGrid()
-c.SetLeftMargin(0.11)
+# Create canvas, receive values for margins
+c, L, R, T, B = utils.create_canvas_wide("c", 0.1, 0.05)
+dataset_legend, dataset_x1 = get_dataset_legend_wide(args.legend, R)
 
 ## find run numbers
 run_numbers = set()
@@ -46,153 +33,54 @@ for histo in histos_list:
     run_numbers.add(int(run_number))
 run_numbers = sorted(run_numbers)
 
-h_eff_vs_run_BMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_eff_vs_run_BMTF.SetMarkerStyle(20)
-h_eff_vs_run_BMTF.SetTitle("")
-h_eff_vs_run_BMTF.GetXaxis().SetTitle("Run Number")
-h_eff_vs_run_BMTF.GetYaxis().SetTitle("Efficiency")
-
-h_eff_vs_run_OMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_eff_vs_run_OMTF.SetMarkerStyle(20)
-h_eff_vs_run_OMTF.SetTitle("")
-h_eff_vs_run_OMTF.GetXaxis().SetTitle("Run Number")
-h_eff_vs_run_OMTF.GetYaxis().SetTitle("Efficiency")
-
-h_eff_vs_run_EMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_eff_vs_run_EMTF.SetMarkerStyle(20)
-h_eff_vs_run_EMTF.SetTitle("")
-h_eff_vs_run_EMTF.GetXaxis().SetTitle("Run Number")
-h_eff_vs_run_EMTF.GetYaxis().SetTitle("Efficiency")
-
-h_eff_vs_run_uGMT = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_eff_vs_run_uGMT.SetMarkerStyle(20)
-h_eff_vs_run_uGMT.SetTitle("")
-h_eff_vs_run_uGMT.GetXaxis().SetTitle("Run Number")
-h_eff_vs_run_uGMT.GetYaxis().SetTitle("Efficiency")
+# Define graph
+h_eff_vs_run = ROOT.TGraphAsymmErrors(len(run_numbers))
 
 TFs = ["uGMT","BMTF","OMTF","EMTF"]
 WPs = ["SingleMu_22"]
 vars = ["phi"]
+pt_l1_label = f"p^{{#mu,L1}}_{{T}} #geq 22 GeV"
+marker_colors = [CMS_color_0, CMS_color_1, CMS_color_2, CMS_color_5]
 
-for index, run_number in enumerate(run_numbers,start=1):
-        for wp in WPs:
-            for var in vars:
-                key =  wp + "_" + str(run_number) + "_" + var
+for i,tf in enumerate(TFs):
+    for index, run_number in enumerate(run_numbers,start=1):
+            for wp in WPs:
+                for var in vars:
 
-                h_passed_BMTF = in_file.Get("BMTF_" + key + "_passed")
-                h_total_BMTF =in_file.Get("BMTF_" + key + "_total")
-                if h_total_BMTF.Integral() != 0:
-                    eff_BMTF = h_passed_BMTF.Integral() / h_total_BMTF.Integral()
-                    eff_err_BMTF = (h_passed_BMTF.Integral() / h_total_BMTF.Integral()) * math.sqrt(h_passed_BMTF.Integral()) / h_total_BMTF.Integral()
-                    h_eff_vs_run_BMTF.SetPoint(index, run_number, eff_BMTF)
-                    h_eff_vs_run_BMTF.SetPointEYlow(index, eff_err_BMTF)
-                    if eff_BMTF + eff_err_BMTF > 1:
-                        h_eff_vs_run_BMTF.SetPointEYhigh(index, 1-eff_BMTF)
+                    key =  tf + "_" + wp + "_" + str(run_number) + "_" + var
+
+                    h_passed = in_file.Get(key + "_passed")
+                    h_total = in_file.Get(key + "_total")
+                    if h_total.Integral() != 0:
+                        eff = h_passed.Integral() / h_total.Integral()
+                        eff_err = (h_passed.Integral() / h_total.Integral()) * math.sqrt(h_passed.Integral()) / h_total.Integral()
+                        h_eff_vs_run.SetPoint(index, run_number, eff)
+                        h_eff_vs_run.SetPointEYlow(index, eff_err)
+                        if eff + eff_err > 1:
+                            h_eff_vs_run.SetPointEYhigh(index, 1-eff)
+                        else:
+                            h_eff_vs_run.SetPointEYhigh(index, eff_err)
                     else:
-                        h_eff_vs_run_BMTF.SetPointEYhigh(index, eff_err_BMTF)
-                else:
-                    eff_BMTF = 0
-                    h_eff_vs_run_BMTF.SetPoint(index, run_number, eff_BMTF)
- 
-                h_passed_OMTF = in_file.Get("OMTF_" + key + "_passed")
-                h_total_OMTF =in_file.Get("OMTF_" + key + "_total")
-                if h_total_OMTF.Integral() != 0:
-                    eff_OMTF = h_passed_OMTF.Integral() / h_total_OMTF.Integral()
-                    eff_err_OMTF = (h_passed_OMTF.Integral() / h_total_OMTF.Integral()) * math.sqrt(h_passed_OMTF.Integral()) / h_total_OMTF.Integral()
-                    h_eff_vs_run_OMTF.SetPoint(index, run_number, eff_OMTF)
-                    h_eff_vs_run_OMTF.SetPointEYlow(index, eff_err_OMTF)
-                    if eff_OMTF + eff_err_OMTF > 1:
-                        h_eff_vs_run_OMTF.SetPointEYhigh(index, 1-eff_OMTF)
-                    else:
-                        h_eff_vs_run_OMTF.SetPointEYhigh(index, eff_err_OMTF)
-                else:
-                    eff_OMTF = 0
-                    h_eff_vs_run_OMTF.SetPoint(index, run_number, eff_OMTF)
+                        eff = 0
+                        h_eff_vs_run.SetPoint(index, run_number, eff)
 
-                h_passed_EMTF = in_file.Get("EMTF_" + key + "_passed")
-                h_total_EMTF =in_file.Get("EMTF_" + key + "_total")
-                if h_total_EMTF.Integral() != 0:
-                    eff_EMTF = h_passed_EMTF.Integral() / h_total_EMTF.Integral()
-                    eff_err_EMTF = (h_passed_EMTF.Integral() / h_total_EMTF.Integral()) * math.sqrt(h_passed_EMTF.Integral()) / h_total_EMTF.Integral()
-                    h_eff_vs_run_EMTF.SetPoint(index, run_number, eff_EMTF)
-                    h_eff_vs_run_EMTF.SetPointEYlow(index, eff_err_EMTF)
-                    if eff_EMTF + eff_err_EMTF > 1:
-                        h_eff_vs_run_EMTF.SetPointEYhigh(index, 1-eff_EMTF)
-                    else:
-                        h_eff_vs_run_EMTF.SetPointEYhigh(index, eff_err_EMTF)             
-                else:
-                    eff_EMTF = 0
-                    h_eff_vs_run_EMTF.SetPoint(index, run_number, eff_EMTF)
+    h_eff_vs_run.SetTitle("")
+    h_eff_vs_run.GetXaxis().SetTitle("Run Number")
+    h_eff_vs_run.GetYaxis().SetTitle("Efficiency")
+    h_eff_vs_run.GetXaxis().SetLimits(min(run_numbers)-10,max(run_numbers)+10)
+    h_eff_vs_run.GetXaxis().SetNoExponent(True)
+    h_eff_vs_run.GetYaxis().SetRangeUser(0.8,1.05)
+    h_eff_vs_run.SetMarkerStyle(31)
+    marker_color = marker_colors[i % len(marker_colors)]  # Cycle through colors
+    h_eff_vs_run.SetMarkerColor(marker_color)
+    h_eff_vs_run.SetLineColor(marker_color)
+    h_eff_vs_run.Draw("AP")
 
-                h_passed_uGMT = in_file.Get("uGMT_" + key + "_passed")
-                h_total_uGMT =in_file.Get("uGMT_" + key + "_total")
-                if h_total_uGMT.Integral() != 0:
-                    eff_uGMT = h_passed_uGMT.Integral() / h_total_uGMT.Integral()
-                    eff_err_uGMT = (h_passed_uGMT.Integral() / h_total_uGMT.Integral()) * math.sqrt(h_passed_uGMT.Integral()) / h_total_uGMT.Integral()
-                    h_eff_vs_run_uGMT.SetPoint(index, run_number, eff_uGMT)
-                    h_eff_vs_run_uGMT.SetPointEYlow(index, eff_err_uGMT)
-                    if eff_uGMT + eff_err_uGMT > 1:
-                        h_eff_vs_run_uGMT.SetPointEYhigh(index, 1-eff_uGMT)
-                    else:
-                        h_eff_vs_run_uGMT.SetPointEYhigh(index, eff_err_uGMT)          
-                else:
-                    eff_uGMT = 0
-                    h_eff_vs_run_uGMT.SetPoint(index, run_number, eff_uGMT)
-
-
-                # print(f"run_number: {run_number}, efF_BMTF: {eff_BMTF}, efF_OMTF: {eff_OMTF}, efF_EMTF: {eff_EMTF}, efF_uGMT: {eff_uGMT}")
-
-h_eff_vs_run_BMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_eff_vs_run_BMTF.GetYaxis().SetRangeUser(0.8,1.05)
-h_eff_vs_run_BMTF.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"BMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "eff_vs_run_BMTF.png")
-
-h_eff_vs_run_OMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_eff_vs_run_OMTF.GetYaxis().SetRangeUser(0.8,1.05)
-h_eff_vs_run_OMTF.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"OMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "eff_vs_run_OMTF.png")
-
-h_eff_vs_run_EMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_eff_vs_run_EMTF.GetYaxis().SetRangeUser(0.8,1.05)
-h_eff_vs_run_EMTF.Draw("AP")
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"EMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "eff_vs_run_EMTF.png")
-
-h_eff_vs_run_uGMT.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_eff_vs_run_uGMT.GetYaxis().SetRangeUser(0.8,1.05)
-h_eff_vs_run_uGMT.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"uGMT")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "eff_vs_run_uGMT.png")
+    utils.add_dataset_legend(dataset_x1,dataset_legend)
+    utils.add_cms_label_in_wide(L,T)
+    latex.DrawLatexNDC(1-R-0.16, B+0.10, pt_l1_label)
+    latex.DrawLatexNDC(1-R-0.18, B+0.05, "L1T Quality #geq 12")
+    latex.SetTextSize(0.04)
+    latex.DrawLatexNDC(1-R-0.09, 1-T-0.07,tf)
+    c.SaveAs(output_dir + f"eff_vs_run_{tf}.png")
+    c.SaveAs(output_dir + f"eff_vs_run_{tf}.pdf")
--- plotters/misid_vs_run_plots.py
+++ plotters/misid_vs_run_plots.py
@@ -1,44 +1,27 @@
 import ROOT
 import argparse
-import utils
 import re
 import math
 import json
+import utils
+from utils import *
 
-ROOT.gStyle.SetOptStat(0)
-ROOT.gStyle.SetLegendBorderSize(0)
-ROOT.gStyle.SetTitleOffset(1.5,"Z")
-ROOT.gStyle.SetPalette(ROOT.kBlueGreenYellow)
-
-latex = ROOT.TLatex()
-latex.SetTextSize(0.04)
-latex.SetTextFont(42)
-ROOT.gStyle.SetLegendTextSize(0.04)
-
-# Colors
-color_0 = ROOT.TColor.GetColor(87,144,252) #blue
-color_1 = ROOT.TColor.GetColor(248,156,32) #orange
-color_2 = ROOT.TColor.GetColor(228,37,54) #red
-color_3 = ROOT.TColor.GetColor(150,74,139) #purple
-color_4 = ROOT.TColor.GetColor(156,156,161) #gray
-color_5 = ROOT.TColor.GetColor(122,33,221) #purple
-
-#parse arguments
+# Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--legend', type=str, help='dataset legend')
 parser.add_argument('-o', type=str, help='output dir')
 parser.add_argument('-i', type=str, help='input dir dir')
 args = parser.parse_args()
 
-dataset_legend = args.legend
+# Pass arguments
 output_dir = args.o
 input_dir = args.i
+# utils.merge_root_files(input_dir)
 
 in_file = ROOT.TFile(input_dir + "merged_total.root","READ")
-c = ROOT.TCanvas("c","c",800,800)
-c.SetLeftMargin(0.11)
-c.SetRightMargin(0.15)
-c.SetGrid()
+# Create canvas, receive values for margins
+c, L, R, T, B = utils.create_canvas_wide("c", 0.1, 0.05)
+dataset_legend, dataset_x1 = get_dataset_legend_wide(args.legend, R)
 
 ## find run numbers
 run_numbers = set()
@@ -49,155 +32,53 @@ for histo in histos_list:
     run_numbers.add(int(run_number))
 run_numbers = sorted(run_numbers)
 
-
-h_misid_vs_run_BMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_misid_vs_run_BMTF.SetMarkerStyle(20)
-h_misid_vs_run_BMTF.SetTitle("")
-h_misid_vs_run_BMTF.GetXaxis().SetTitle("Run Number")
-h_misid_vs_run_BMTF.GetYaxis().SetTitle("Charge misidentification")
-
-h_misid_vs_run_OMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_misid_vs_run_OMTF.SetMarkerStyle(20)
-h_misid_vs_run_OMTF.SetTitle("")
-h_misid_vs_run_OMTF.GetXaxis().SetTitle("Run Number")
-h_misid_vs_run_OMTF.GetYaxis().SetTitle("Charge misidentification")
-
-h_misid_vs_run_EMTF = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_misid_vs_run_EMTF.SetMarkerStyle(20)
-h_misid_vs_run_EMTF.SetTitle("")
-h_misid_vs_run_EMTF.GetXaxis().SetTitle("Run Number")
-h_misid_vs_run_EMTF.GetYaxis().SetTitle("Charge misidentification")
-
-h_misid_vs_run_uGMT = ROOT.TGraphAsymmErrors(len(run_numbers))
-h_misid_vs_run_uGMT.SetMarkerStyle(20)
-h_misid_vs_run_uGMT.SetTitle("")
-h_misid_vs_run_uGMT.GetXaxis().SetTitle("Run Number")
-h_misid_vs_run_uGMT.GetYaxis().SetTitle("Charge misidentification")
+# Define graph
+h_misid_vs_run = ROOT.TGraphAsymmErrors(len(run_numbers))
 
 TFs = ["uGMT","BMTF","OMTF","EMTF"]
 WPs = ["SingleMu_22"]
 vars = ["phi"]
+pt_l1_label = f"p^{{#mu,L1}}_{{T}} #geq 22 GeV"
+marker_colors = [CMS_color_0, CMS_color_1, CMS_color_2, CMS_color_5]
 
-for index, run_number in enumerate(run_numbers,start=1):
-        for wp in WPs:
-            for var in vars:
-                key =  wp + "_" + str(run_number) + "_" + var
+for i,tf in enumerate(TFs):
+    for index, run_number in enumerate(run_numbers,start=1):
+            for wp in WPs:
+                for var in vars:
+                    key =  tf + "_" + wp + "_" + str(run_number) + "_" + var
 
-                h_passed_BMTF = in_file.Get("BMTF_" + key + "_passed")
-                h_total_BMTF =in_file.Get("BMTF_" + key + "_total")
-                if h_total_BMTF.Integral() != 0:
-                    misid_BMTF = h_passed_BMTF.Integral() / h_total_BMTF.Integral()
-                    misid_err_BMTF = (h_passed_BMTF.Integral() / h_total_BMTF.Integral()) * math.sqrt(h_passed_BMTF.Integral()) / h_total_BMTF.Integral()
-                    h_misid_vs_run_BMTF.SetPoint(index, run_number, misid_BMTF)
-                    h_misid_vs_run_BMTF.SetPointEYlow(index, misid_err_BMTF)
-                    if misid_BMTF + misid_err_BMTF > 1:
-                        h_misid_vs_run_BMTF.SetPointEYhigh(index, 1-misid_BMTF)
+                    h_passed = in_file.Get(key + "_passed")
+                    h_total = in_file.Get(key + "_total")
+                    if h_total.Integral() != 0:
+                        misid = h_passed.Integral() / h_total.Integral()
+                        misid_err = (h_passed.Integral() / h_total.Integral()) * math.sqrt(h_passed.Integral()) / h_total.Integral()
+                        h_misid_vs_run.SetPoint(index, run_number, misid)
+                        h_misid_vs_run.SetPointEYlow(index, misid_err)
+                        if misid + misid_err > 1:
+                            h_misid_vs_run.SetPointEYhigh(index, 1-misid)
+                        else:
+                            h_misid_vs_run.SetPointEYhigh(index, misid_err)
                     else:
-                        h_misid_vs_run_BMTF.SetPointEYhigh(index, misid_err_BMTF)
-                else:
-                    misid_BMTF = 0
-                    h_misid_vs_run_BMTF.SetPoint(index, run_number, misid_BMTF)
- 
-                h_passed_OMTF = in_file.Get("OMTF_" + key + "_passed")
-                h_total_OMTF =in_file.Get("OMTF_" + key + "_total")
-                if h_total_OMTF.Integral() != 0:
-                    misid_OMTF = h_passed_OMTF.Integral() / h_total_OMTF.Integral()
-                    misid_err_OMTF = (h_passed_OMTF.Integral() / h_total_OMTF.Integral()) * math.sqrt(h_passed_OMTF.Integral()) / h_total_OMTF.Integral()
-                    h_misid_vs_run_OMTF.SetPoint(index, run_number, misid_OMTF)
-                    h_misid_vs_run_OMTF.SetPointEYlow(index, misid_err_OMTF)
-                    if misid_OMTF + misid_err_OMTF > 1:
-                        h_misid_vs_run_OMTF.SetPointEYhigh(index, 1-misid_OMTF)
-                    else:
-                        h_misid_vs_run_OMTF.SetPointEYhigh(index, misid_err_OMTF)
-                else:
-                    misid_OMTF = 0
-                    h_misid_vs_run_OMTF.SetPoint(index, run_number, misid_OMTF)
+                        misid = 0
+                        h_misid_vs_run.SetPoint(index, run_number, misid)
+    
+    h_misid_vs_run.SetTitle("")
+    h_misid_vs_run.GetXaxis().SetTitle("Run Number")
+    h_misid_vs_run.GetYaxis().SetTitle("Charge misidentification")
+    h_misid_vs_run.GetXaxis().SetLimits(min(run_numbers)-10,max(run_numbers)+10)
+    h_misid_vs_run.GetXaxis().SetNoExponent(True)
+    h_misid_vs_run.GetYaxis().SetRangeUser(0.,0.1)
+    h_misid_vs_run.SetMarkerStyle(31)
+    marker_color = marker_colors[i % len(marker_colors)]  # Cycle through colors
+    h_misid_vs_run.SetMarkerColor(marker_color)
+    h_misid_vs_run.SetLineColor(marker_color)
+    h_misid_vs_run.Draw("AP")
 
-                h_passed_EMTF = in_file.Get("EMTF_" + key + "_passed")
-                h_total_EMTF =in_file.Get("EMTF_" + key + "_total")
-                if h_total_EMTF.Integral() != 0:
-                    misid_EMTF = h_passed_EMTF.Integral() / h_total_EMTF.Integral()
-                    misid_err_EMTF = (h_passed_EMTF.Integral() / h_total_EMTF.Integral()) * math.sqrt(h_passed_EMTF.Integral()) / h_total_EMTF.Integral()
-                    h_misid_vs_run_EMTF.SetPoint(index, run_number, misid_EMTF)
-                    h_misid_vs_run_EMTF.SetPointEYlow(index, misid_err_EMTF)
-                    if misid_EMTF + misid_err_EMTF > 1:
-                        h_misid_vs_run_EMTF.SetPointEYhigh(index, 1-misid_EMTF)
-                    else:
-                        h_misid_vs_run_EMTF.SetPointEYhigh(index, misid_err_EMTF)             
-                else:
-                    misid_EMTF = 0
-                    h_misid_vs_run_EMTF.SetPoint(index, run_number, misid_EMTF)
-
-                h_passed_uGMT = in_file.Get("uGMT_" + key + "_passed")
-                h_total_uGMT =in_file.Get("uGMT_" + key + "_total")
-                if h_total_uGMT.Integral() != 0:
-                    misid_uGMT = h_passed_uGMT.Integral() / h_total_uGMT.Integral()
-                    misid_err_uGMT = (h_passed_uGMT.Integral() / h_total_uGMT.Integral()) * math.sqrt(h_passed_uGMT.Integral()) / h_total_uGMT.Integral()
-                    h_misid_vs_run_uGMT.SetPoint(index, run_number, misid_uGMT)
-                    h_misid_vs_run_uGMT.SetPointEYlow(index, misid_err_uGMT)
-                    if misid_uGMT + misid_err_uGMT > 1:
-                        h_misid_vs_run_uGMT.SetPointEYhigh(index, 1-misid_uGMT)
-                    else:
-                        h_misid_vs_run_uGMT.SetPointEYhigh(index, misid_err_uGMT)          
-                else:
-                    misid_uGMT = 0
-                    h_misid_vs_run_uGMT.SetPoint(index, run_number, misid_uGMT)
-
-
-                # print(f"run_number: {run_number}, misid_BMTF: {misid_BMTF}, misid_OMTF: {misid_OMTF}, misid_EMTF: {misid_EMTF}, misid_uGMT: {misid_uGMT}")
-
-h_misid_vs_run_BMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_misid_vs_run_BMTF.GetYaxis().SetRangeUser(0.,0.1)
-h_misid_vs_run_BMTF.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"BMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "misid_vs_run_BMTF.png")
-
-h_misid_vs_run_OMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_misid_vs_run_OMTF.GetYaxis().SetRangeUser(0.,0.1)
-h_misid_vs_run_OMTF.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"OMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "misid_vs_run_OMTF.png")
-
-h_misid_vs_run_EMTF.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_misid_vs_run_EMTF.GetYaxis().SetRangeUser(0.,0.1)
-h_misid_vs_run_EMTF.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"EMTF")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "misid_vs_run_EMTF.png")
-
-h_misid_vs_run_uGMT.GetXaxis().SetLimits(min(run_numbers),max(run_numbers))
-h_misid_vs_run_uGMT.GetYaxis().SetRangeUser(0.,0.1)
-h_misid_vs_run_uGMT.Draw("AP")
-latex.SetTextSize(0.04)
-latex.DrawLatexNDC(0.80,0.91,dataset_legend)
-latex.DrawLatexNDC(0.76,0.86,"uGMT")
-# latex.DrawLatexNDC(0.14, 0.86, "Reco p_{T} #geq 26 GeV")
-latex.DrawLatexNDC(0.14, 0.86, "L1 Qual #geq 12")
-latex.SetTextSize(0.045)
-latex.DrawLatexNDC(0.1, 0.91, "#font[61]{CMS}")
-latex.SetTextSize(0.0346)
-latex.DrawLatexNDC(0.195, 0.91, "#font[52]{Internal}")
-c.SaveAs(output_dir + "misid_vs_run_uGMT.png")
+    utils.add_dataset_legend(dataset_x1,dataset_legend)
+    utils.add_cms_label_out_wide(L,T)
+    latex.DrawLatexNDC(1-R-0.155, 1-T-0.11, pt_l1_label)
+    latex.DrawLatexNDC(1-R-0.18, 1-T-0.155, "L1T Quality #geq 12")
+    latex.SetTextSize(0.04)
+    latex.DrawLatexNDC(1-R-0.09, 1-T-0.05,tf)
+    c.SaveAs(output_dir + f"misid_vs_run_{tf}.png")
+    c.SaveAs(output_dir + f"misid_vs_run_{tf}.pdf")
